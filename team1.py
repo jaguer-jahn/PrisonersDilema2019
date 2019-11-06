1af5947fbdf4900715cd4322bffaa53663c22f3b
Jaguer Heier <jaguer124@gmail.com>
2019-11-06 08:30:58 -0600

    Update team1.py

--- team1.py
+++ team1.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Gamblers' # Only 10 chars displayed.
+strategy_name = 'Statistical Logic'
+strategy_description = 'Bases off of other players moves and weighs odds against his future moves'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
